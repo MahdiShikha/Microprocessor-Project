5fad176c9094d7f4730ca7b476d53a10e19b1fb5
cid-02394536 <mahdi97779@gmail.com>
2025-12-11 11:18:14 +0000

    24bit ADC setup and main

--- ACD.s
+++ ACD.s
@@ -1,27 +1,144 @@
+; ============================================================
+; File: ADC24.s
+; Description: SPI driver for AMC131M03 24-bit ADC (ISO ADC 7 click)
+; PIC18F87K22 ? mikroBUS pins:
+; CS  = RE0, SCK = RC3, SDI = RC4, SDO = RC5, XEN = RC1, DRDY = RB0
+; ============================================================
+
 #include <xc.inc>
 
-global  ADC_Setup, ADC_Read    
-    
-psect	adc_code, class=CODE
-    
-ADC_Setup:
-	bsf	TRISA, PORTA_RA0_POSN, A  ; pin RA0==AN0 input
-	movlb	0x0f
-	bsf	ANSEL0	    ; set AN0 to analog
-	movlb	0x00
-	movlw   0x01	    ; select AN0 for measurement
-	movwf   ADCON0, A   ; and turn ADC on
-	movlw   0x30	    ; Select 4.096V positive reference
-	movwf   ADCON1,	A   ; 0V for -ve reference and -ve input
-	movlw   0xF6	    ; Right justified output
-	movwf   ADCON2, A   ; Fosc/64 clock and acquisition times
-	return
+global  ADC24_Setup, ADC24_Read
+global  ADC24_H, ADC24_M, ADC24_L
 
-ADC_Read:
-	bsf	GO	    ; Start conversion by setting GO bit in ADCON0
-adc_loop:
-	btfsc   GO	    ; check to see if finished
-	bra	adc_loop
-	return
+psect   udata_acs
+ADC24_H:    ds 1      ; 24-bit result MSB
+ADC24_M:    ds 1      ; middle byte
+ADC24_L:    ds 1      ; LSB
+
+psect   adc24_code, class=CODE
+
+; ============================================================
+;  Subroutine: ADC24_Setup
+; ============================================================
+ADC24_Setup:
+    ; ----- Configure SPI and control pins -----
+    bcf     TRISE, 0, A     ; RE0 (CS) output
+    bcf     TRISC, 1, A     ; RC1 (XEN) output
+    bsf     TRISB, 0, A     ; RB0 (DRDY) input
+    bcf     TRISC, 3, A     ; RC3 (SCK) output
+    bsf     TRISC, 4, A     ; RC4 (SDI) input
+    bcf     TRISC, 5, A     ; RC5 (SDO) output
+
+    ; Idle states
+    bsf     LATE, 0, A      ; CS high
+    bsf     LATC, 1, A      ; XEN high (enable oscillator)
+
+    ; ----- Configure SPI (MSSP1) -----
+    ; SPI Master mode, Fosc/16, CKP=0, CKE=1
+    movlw   0b00100010
+    movwf   SSP1CON1, A
+    movlw   0b01000000
+    movwf   SSP1STAT, A
+    bsf     SSPEN, A        ; enable SPI
+
+    ; ----- Delay for ADC power-up -----
+    movlw   d'100'
+    call    delay_ms_24adc
+
+    ; ----- Configure AMC131M03 registers -----
+    ; Clock Control Register (0x01): CH0 enabled, internal clock, 1 MHz output
+    movlw   0x01        ; register address
+    movwf   FSR2L, A
+    movlw   0x00        ; CH0_EN=1, CLKSEL=internal, OSR=512 (default)
+    call    ADC24_Write_Reg
+
+    ; CH0_CFG register (0x03): Gain=1, bipolar, input = AIN0P/AIN0N
+    movlw   0x03
+    movwf   FSR2L, A
+    movlw   0x00
+    call    ADC24_Write_Reg
+
+    ; POWER_CFG register (0x05): normal power mode
+    movlw   0x05
+    movwf   FSR2L, A
+    movlw   0x00
+    call    ADC24_Write_Reg
+
+    return
+
+; ============================================================
+; Subroutine: ADC24_Read
+; Waits for DRDY low, then clocks out 3 bytes from ADC
+; ============================================================
+ADC24_Read:
+wait_DRDY:
+    btfsc   PORTB, 0, A     ; Wait until RB0 (DRDY) = 0
+    bra     wait_DRDY
+
+    bcf     LATE, 0, A      ; CS low
+    call    SPI_Read_Byte
+    movwf   ADC24_H, A
+
+    call    SPI_Read_Byte
+    movwf   ADC24_M, A
+
+    call    SPI_Read_Byte
+    movwf   ADC24_L, A
+
+    bsf     LATE, 0, A      ; CS high
+    return
+
+; ============================================================
+; Helper: Write one configuration register
+; Input:  register address in FSR2L, data in W
+; ============================================================
+ADC24_Write_Reg:
+    movwf   PRODH, A        ; Save data byte in PRODH
+    bcf     LATE, 0, A      ; CS low
+
+    ; Send write command: 0x40 | (reg addr)
+    movf    FSR2L, W, A
+    iorlw   0x40
+    call    SPI_Write_Byte
+
+    ; Send data byte
+    movf    PRODH, W, A
+    call    SPI_Write_Byte
+
+    bsf     LATE, 0, A      ; CS high
+    return
+
+; ============================================================
+; SPI Byte Write
+; ============================================================
+SPI_Write_Byte:
+    movwf   SSP1BUF, A
+wait_spi_tx:
+    btfss   SSP1STAT, BF
+    bra     wait_spi_tx
+    movf    SSP1BUF, W, A
+    return
+
+; ============================================================
+; SPI Byte Read (dummy write)
+; ============================================================
+SPI_Read_Byte:
+    movlw   0x00
+    movwf   SSP1BUF, A
+wait_spi_rx:
+    btfss   SSP1STAT, BF
+    bra     wait_spi_rx
+    movf    SSP1BUF, W, A
+    return
+
+; ============================================================
+; Delay (approx ms) for ADC startup
+; ============================================================
+delay_ms_24adc:
+    movwf   TMR0L, A    ; crude wait loop
+delay_loop_24adc:
+    decfsz  TMR0L, A
+    bra     delay_loop_24adc
+    return
 
 end
--- main.s
+++ main.s
@@ -1,55 +1,60 @@
+; ============================================================
+; File: main.s
+; Description: Example main program for AMC131M03 ADC readout
+; ============================================================
+
 #include <xc.inc>
 
-extrn	UART_Setup, UART_Transmit_Message  ; external uart subroutines
-extrn	LCD_Setup, LCD_Write_Message, LCD_Write_Hex,LCD_Send_Byte_D ; external LCD subroutines
-extrn	ADC_Setup, ADC_Read		   ; external ADC subroutines
-extrn	Mul16x16,Mul24x8
-extrn	ARG1L,ARG1H,ARG2L,ARG2H
-extrn	X0,X1,X2,Y0
-extrn	RES0,RES1,RES2,RES3	
-extrn	ADC_to_4digits
-extrn	DEC3,DEC2,DEC1,DEC0
-psect	udata_acs   ; reserve data space in access ram
-counter:    ds 1    ; reserve one byte for a counter variable
-delay_count:ds 1    ; reserve one byte for counter in the delay routine
-    
-psect	udata_bank4 ; reserve data anywhere in RAM (here at 0x400)
-myArray:    ds 0x80 ; reserve 128 bytes for message data
+extrn	UART_Setup, UART_Transmit_Message, UART_Transmit_Byte
+extrn	LCD_Setup, LCD_Write_Hex
+extrn	ADC24_Setup, ADC24_Read
+extrn	ADC24_H, ADC24_M, ADC24_L
 
+psect	udata_acs
+counter:    ds 1
+delay_count:ds 1
 
-psect	data    
-	; ******* myTable, data in programme memory, and its length *****
+psect	data
 myTable:
-	db	'H','e','l','l','o',' ','W','o','r','l','d','!',0x0a
-					; message, plus carriage return
-	myTable_l   EQU	13	; length of data
-	align	2
-    
-psect	code, abs	
+	db	'A','D','C','2','4',':',0x0A
+myTable_l   EQU	7
+
+psect	code, abs
 rst: 	org 0x0
- 	goto	setup
+	goto	setup
 
-	; ******* Programme FLASH read Setup Code ***********************
-setup:	bcf	CFGS	; point to Flash program memory  
-	bsf	EEPGD 	; access Flash program memory
-	call	UART_Setup	; setup UART
-	call	LCD_Setup	; setup UART
-	call	ADC_Setup	; setup ADC
-	goto	measure_loop
-;measure_loop:
-	;call    ADC_Read          ; ADRESH:ADRESL now contain 12-bit code
+setup:
+	bcf	CFGS
+	bsf	EEPGD
 
-measure_loop:
-	call	ADC_Read
-	movf	ADRESH, W, A
+	call	UART_Setup
+	call	LCD_Setup
+	call	ADC24_Setup
+
+	goto	main_loop
+
+; ============================================================
+; Continuous measurement loop
+; ============================================================
+main_loop:
+	call	ADC24_Read
+
+	; Display on LCD
+	movf	ADC24_H, W, A
 	call	LCD_Write_Hex
-	movf	ADRESL, W, A
+	movf	ADC24_M, W, A
 	call	LCD_Write_Hex
-	goto	measure_loop		; goto current line in code
-	
-	; a delay subroutine if you need one, times around loop in delay_count
-delay:	decfsz	delay_count, A	; decrement until zero
-	bra	delay
-	return
+	movf	ADC24_L, W, A
+	call	LCD_Write_Hex
 
-	end	rst
+	; Transmit to PC via UART
+	movf	ADC24_H, W, A
+	call	UART_Transmit_Byte
+	movf	ADC24_M, W, A
+	call	UART_Transmit_Byte
+	movf	ADC24_L, W, A
+	call	UART_Transmit_Byte
+
+	goto	main_loop
+
+end	rst
